f7a605f185dd6778d22ad4d12e06bf638e8e0616
Simone Rubino <sir@takobi.online>
2022-04-21 10:18:57 +0200

    [MIG] account_invoice_line_default_account: Migration to 12.0 This module is not compatible with account_invoice_line_sequence because them both add to the context of invoice lines in invoice form view

--- account_invoice_line_default_account/models/account_invoice_line.py
+++ account_invoice_line_default_account/models/account_invoice_line.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2012 Therp BV (<http://therp.nl>)
 # Copyright 2013-2018 BCIM SPRL (<http://www.bcim.be>)
+# Copyright 2022 Simone Rubino - TAKOBI
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
 
@@ -9,12 +10,12 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     def _account_id_default(self):
-        partner_id = self._context.get('partner_id')
+        partner_id = self.env.context.get('partner_id')
         if not partner_id:
             return self._default_account()
         assert isinstance(partner_id, int), (
             _('No valid id for context partner_id %d') % partner_id)
-        invoice_type = self._context.get('type')
+        invoice_type = self.env.context.get('type')
         if invoice_type in ['in_invoice', 'in_refund']:
             partner = self.env['res.partner'].browse(partner_id)
             if partner.property_account_expense:
@@ -30,18 +31,20 @@ class AccountInvoiceLine(models.Model):
     @api.onchange('account_id')
     def _onchange_account_id(self):
         if not self.account_id or not self.partner_id or self.product_id:
-            return super(AccountInvoiceLine, self)._onchange_account_id()
-        if self._context.get('journal_id'):
+            return super()._onchange_account_id()
+        if self.env.context.get('journal_id'):
             journal = self.env['account.journal'].browse(
-                self._context.get('journal_id'))
+                self.env.context.get('journal_id'))
             if self.account_id in [
                     journal.default_credit_account_id,
                     journal.default_debit_account_id]:
-                return super(AccountInvoiceLine, self)._onchange_account_id()
+                return super()._onchange_account_id()
         # We have a manually entered account_id (no product_id, so the
         # account_id is not the result of a product selection).
         # Store this account_id as future default in res_partner.
-        inv_type = self._context.get('type', 'out_invoice')
+        # `write`ing in an onchange is bad,
+        # but `update`ing the partner does not work.
+        inv_type = self.env.context.get('type', 'out_invoice')
         if (inv_type in ['in_invoice', 'in_refund'] and
                 self.partner_id.auto_update_account_expense):
             if self.account_id != self.partner_id.property_account_expense:
@@ -52,4 +55,4 @@ class AccountInvoiceLine(models.Model):
             if self.account_id != self.partner_id.property_account_income:
                 self.partner_id.write({
                     'property_account_income': self.account_id.id})
-        return super(AccountInvoiceLine, self)._onchange_account_id()
+        return super()._onchange_account_id()
